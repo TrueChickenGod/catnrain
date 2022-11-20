7cabf7275fa4b76a3b96ca17cb3af387335ddb8f
jpenlan <79890843+jpenlan@users.noreply.github.com>
2022-11-19 17:36:21 -0800

    Prototype WIP

---
+++ Team_Project_Prototype-Team_12/js/lab.js
@@ -0,0 +1,16 @@
+/*
+ * Author(s): Adrian Cheng <mcheng32@ucsc.edu>
+              Isabella Quinn-Rodriguez <iquinnro@ucsc.edu>
+              Jacob Penland <jrpenlan@ucsc.edu>
+              Ramola Datta <rsdatta@ucsc.edu>
+              Travis Carlen <tcarlen@ucsc.edu>
+ * Created: Nov. 15, 2022
+ * Description: Currently a working prototype for our website, will adjust and refine as time goes on.
+ */
+
+
+
+
+
+
+ 
