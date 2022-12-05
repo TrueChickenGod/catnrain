68c131c11933d1f92df5b098962fca7181afd1b8
jpenlan <79890843+jpenlan@users.noreply.github.com>
2022-12-05 02:17:32 -0800

    DONE

--- components/laptop.js
+++ components/laptop.js
@@ -3,11 +3,25 @@ function show2(){
   return;
 }
 
+var check;
+
 $("#computer ").click(show2);
 $("#audio").click(function(){
   show2();
+  if (document.getElementById('rain').paused) {
+      document.getElementById('rain').play();
+  }
+  else {
+    document.getElementById('rain').pause();
+  }
 });
 
 $("#music").click(function(){
   show2();
+  if (document.getElementById('lofi').paused) {
+      document.getElementById('lofi').play();
+  }
+  else {
+    document.getElementById('lofi').pause();
+  }
 });
